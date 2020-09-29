f9c71e039cd51c4480140e5976abc22f08f6011a
christian <christian.weber@leftshift.one>
2020-09-29 15:56:07 +0200

    [CI] tests fixed

--- explicit-java/src/test/kotlin/explicit/token/tokenizer/CharacterCaseTokenizerTest.kt
+++ explicit-java/src/test/kotlin/explicit/token/tokenizer/CharacterCaseTokenizerTest.kt
@@ -3,6 +3,7 @@ package explicit.token.tokenizer
 import explicit.parser.xml.ExplicitXmlParser
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.io.FileInputStream
 
 class CharacterCaseTokenizerTest {
 
@@ -33,7 +34,7 @@ class CharacterCaseTokenizerTest {
     @Test
     fun testTokenizerWithCustomTokens() {
         val tokenizer = CharacterCaseTokenizer()
-        val rulez = ExplicitXmlParser().parse(javaClass.getResourceAsStream("/de/price.xml"))
+        val rulez = ExplicitXmlParser().parse(FileInputStream(System.getProperty("user.dir") + "/../src/main/resources/de/price.xml"))
         Assertions.assertEquals(listOf("bis", "1.000", "€", ".").toString(), tokenizer.tokenize("bis 1.000€.", rulez.tokens).toString())
         Assertions.assertEquals(listOf("bis", "1.000,-", "€").toString(), tokenizer.tokenize("bis 1.000,- €", rulez.tokens).toString())
     }
@@ -41,8 +42,8 @@ class CharacterCaseTokenizerTest {
     @Test
     fun testTokenizerWithDatetimeTokens() {
         val tokenizer = CharacterCaseTokenizer()
-        val rulez = ExplicitXmlParser().parse(javaClass.getResourceAsStream("/de/datetime.xml"))
-        Assertions.assertEquals(listOf("ich", "suche", "von", "9.2.", "bis", "15/16.2", "2018").toString(), tokenizer.tokenize("ich suche von 9.2. bis 15/16.2 2018", rulez.tokens).toString())
+        val rulez = ExplicitXmlParser().parse(FileInputStream(System.getProperty("user.dir") + "/../src/main/resources/de/datetime.xml"))
+        Assertions.assertEquals(listOf("ich", "suche", "von", "9.2.", "bis", "16.2", "2018").toString(), tokenizer.tokenize("ich suche von 9.2. bis 15/16.2 2018", rulez.tokens).toString())
     }
 
 
