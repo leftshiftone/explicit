ea03801b7f4d48065c3eee32f32f94e9272ceb9b
alberto.geaperez <alberto.geaperez@leftshift.one>
2020-11-03 15:09:36 +0100

    GAIA-2253 Adapt test with the proper path to testFiles

--- explicit-python/explicit_nlu/parser/xml/__test__/XmlExplicitRulesParserTest.py
+++ explicit-python/explicit_nlu/parser/xml/__test__/XmlExplicitRulesParserTest.py
@@ -1,14 +1,16 @@
 import unittest
+import os
 
 from explicit_nlu.parser.xml.ExplicitXmlParser import ExplicitXmlParser
 
 
 class XmlExplicitRulesParserTest(unittest.TestCase):
 
+    root_dir = os.path.dirname(os.path.realpath(__file__))
+
     def test_tokenization(self):
         parser = ExplicitXmlParser()
-        rulez = parser.parse("D:/IntellijProjects/nerulez/src/main/resources/de/datetime.xml")
-
+        rulez = parser.parse(f"{self.root_dir}/../../../../../src/main/resources/de/datetime.xml")
         self.assertTrue(len(rulez.mappings) > 0)
         self.assertTrue(len(rulez.rulez) > 0)
         self.assertTrue(len(rulez.patterns) > 0)
